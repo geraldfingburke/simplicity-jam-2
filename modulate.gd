0aff5070cebaf8b25a69310644c3275418e2bd7a
geraldfingburke <geraldfingburke@gmail.com>
2024-12-06 11:55:02 -0500

    Add torch, set up hud

--- modulate.gd
+++ modulate.gd
@@ -1,7 +1,7 @@
 extends Sprite2D
 
-@export var min : float
-@export var max : float
+@export var min_diameter : float
+@export var max_diameter : float
 @export var speed : float
 var growing = true
 
@@ -12,9 +12,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if scale >= Vector2(max, max):
+	if scale >= Vector2(max_diameter, max_diameter):
 		growing = false
-	if scale <= Vector2(min, min):
+	if scale <= Vector2(min_diameter, min_diameter):
 		growing = true
 	if growing:
 		scale += Vector2(1,1) * speed * delta
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,7 +3,7 @@ extends Area2D
 @export var health = 10
 @export var health_max = 10
 @export var attack = 1
-@export var torch = 10
+@export var torch = 0
 @export var torch_min = 10
 
 var tile_size = 16
@@ -15,7 +15,9 @@ var is_moving = false
 var is_lit = false
 
 @onready var ray = $RayCast2D
-@onready var torch_sprite = $PlayerSprite/CanvasModulate/Sprite2D
+@onready var torch_sprite = $CanvasModulate/Sprite2D
+@onready var stats_label = $Control/StatsLabel
+@onready var alert_label = $Control/AlertLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,6 +31,7 @@ func _unhandled_input(event):
 			move(dir)
 
 func move(dir):
+	clear_label()
 	ray.target_position = inputs[dir] * tile_size
 	ray.force_raycast_update()
 	if !ray.is_colliding():
@@ -37,5 +40,45 @@ func move(dir):
 		is_moving = true
 		await tween.finished
 		is_moving = false
+		update_light()
+		update_stats_label()
 	else:
-		var object = ray.get_collider().get_parent()
+		var object = ray.get_collider()
+		match object.get_meta("Item"):
+			"Torch":
+				print("Here")
+				pickup_torch()
+				object.queue_free()
+				return
+
+func update_light():
+	if torch > 0:
+		torch -= 1
+		light()
+	if torch <= 0:
+		is_lit = false
+		light()
+
+func pickup_torch():
+	update_alert_label("[center]You found a torch[/center]")
+	is_lit = true
+	torch += torch_min
+	light()
+	update_stats_label()
+
+func light():
+	if (is_lit):
+		torch_sprite.min_diameter = 3
+		torch_sprite.max_diameter = 3.1
+	else:
+		torch_sprite.min_diameter = 1
+		torch_sprite.max_diameter = 1.1
+
+func clear_label():
+	alert_label.text = ""
+
+func update_alert_label(text):
+	alert_label.text = text
+
+func update_stats_label():
+	stats_label.text = "Health: %s Attack: %s Light: %s" % [health, attack, torch]
